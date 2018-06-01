fffd13132113d1718cd8536649470554ce00f52a
hollandjames <38698414+hollandjames@users.noreply.github.com>
2018-06-01 04:13:50 +0100

    bootstrap and move logic

---
+++ js/variables.js
@@ -0,0 +1,14 @@
+var solutionOne = ['d6d8','c4f4','g2g4','f5g4','d8h4'];
+var solutionTwo =['e4d5'];
+var solutionThree =['c1e2','g1f1','g7f6','e5f6','e8b8','f1e2','b2b1'];
+var solutionFour = ['f5f4','d6d5','h5h4','d5e4','f4f3','g2f3','h4h3'];
+
+var allSolutions=[solutionOne,solutionTwo,solutionThree,solutionFour];
+var players = ["Kasparov-Mamedyarov", "Beliavsky-Timman","Kristiansen-Berg, Aarbus, 1990","Pomar-Cuadras, Olot, 1974"];
+
+var puzzleArray =
+['6R1/5q1p/3Qp1pk/p3Pp2/Ppr2P2/2P4P/6PK/8 w - - 0 1',
+'rnbqkbnr/ppp1p1pp/8/3p1p2/4P3/2P5/PP1P1PPP/RNBQKBNR w KQkq - 0 3',
+'4r1k1/6qp/p3pRp1/4B3/3P4/8/1pQ2PPP/2n3K1 b - - 0 1',
+'8/8/3K1k2/5p1p/4p1p1/4P1P1/5PP1/8 b - - 0 1'
+];
